58c6dc5af57050d230a3969d2d1b2573f4c546f2
Clay Buxton <cbuxton7@gatech.edu>
2022-02-09 22:42:57 -0500

    base work

---
+++ GearSetter/Plugin.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Numerics;
+using ImGuiNET;
+
+using Dalamud.Interface;
+
+using Dalamud.Data;
+using Dalamud.Game;
+using Dalamud.Game.ClientState;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+
+using Lumina.Excel;
+using Lumina.Excel.GeneratedSheets;
+
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Component.Excel;
+
+
+namespace GearSetter
+{
+
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "GearSetter";
+
+        private const string commandName = "/gs";
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private CommandManager CommandManager { get; init; }
+        private Configuration Configuration { get; init; }
+        private PluginUI PluginUi { get; init; }
+
+        private DataManager DataManager { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] CommandManager commandManager,
+            DataManager dataManager
+            )
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+            this.DataManager = dataManager;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+
+            // you might normally want to embed resources and load them from the manifest stream
+            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+            this.PluginUi = new PluginUI(this.Configuration, goatImage);
+
+            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "schleppin"
+            });
+
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+        }
+
+
+
+        public void Dispose()
+        {
+            this.PluginUi.Dispose();
+            this.CommandManager.RemoveHandler(commandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            this.PluginUi.Visible = true;
+
+        }
+
+
+        private unsafe void DrawUI()
+        {
+
+
+            var inventoryManager = InventoryManager.Instance();
+            var inventory = inventoryManager->GetInventoryContainer(InventoryType.Inventory1);
+            var itemCSV = DataManager.GetExcelSheet<Item>()!;
+
+   
+            ImGui.SetNextWindowSize(new Vector2(375, 330), ImGuiCond.FirstUseEver);
+            ImGui.SetNextWindowSizeConstraints(new Vector2(375, 330), new Vector2(float.MaxValue, float.MaxValue));
+            ImGui.Begin("Inventory", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse);
+                for (int i = 0; i < inventory->Size; i++)
+                {
+                    ImGui.Text($"Item ID: {inventory->Items[i].ItemID}");
+
+
+                    
+               
+                }
+            ImGui.End();
+        }
+
+        private void DrawConfigUI()
+        {
+            this.PluginUi.SettingsVisible = true;
+        }
+    }
+}
