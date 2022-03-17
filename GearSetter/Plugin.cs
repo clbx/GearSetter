9099bfccfc2ae56bb9adae6b4031070668bed31d
Clay Buxton <cbuxton7@gatech.edu>
2022-03-17 18:13:23 -0400

    keep forgetting to push

--- GearSetter/Plugin.cs
+++ GearSetter/Plugin.cs
@@ -23,6 +23,9 @@ using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using FFXIVClientStructs.FFXIV.Component.Excel;
 
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using FFXIVClientStructs.FFXIV.Client.Graphics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace GearSetter
 {
@@ -36,34 +39,32 @@ namespace GearSetter
         private DalamudPluginInterface PluginInterface { get; init; }
         private CommandManager CommandManager { get; init; }
         private Configuration Configuration { get; init; }
-
+        private GameGui GameGui { get; init; }
         private DataManager DataManager { get; init; }
+
+        public GameGui gui; 
 
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
             [RequiredVersion("1.0")] CommandManager commandManager,
-            DataManager dataManager
+            DataManager dataManager,
+            GameGui gameGui
             )
         {
             this.PluginInterface = pluginInterface;
             this.CommandManager = commandManager;
             this.DataManager = dataManager;
+            this.GameGui = gameGui;
 
             this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
             this.Configuration.Initialize(this.PluginInterface);
 
-            // you might normally want to embed resources and load them from the manifest stream
-            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
-            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
 
-            /*
-            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
-            {
-                HelpMessage = "schleppin"
-            });
-            */
+            gui = this.GameGui;
+
 
             this.PluginInterface.UiBuilder.Draw += DrawUI;
+
         }
 
 
@@ -72,7 +73,7 @@ namespace GearSetter
         {
             this.CommandManager.RemoveHandler(commandName);
         }
-        
+
         /**
         private void OnCommand(string command, string args)
         {
@@ -81,7 +82,18 @@ namespace GearSetter
 
         }**/
 
-
+        private unsafe AtkResNode* FindNodeByID(int id, AtkUnitBase* unitBase)
+        {
+            for (int i = 0; i < unitBase->UldManager.NodeListCount; i++)
+            {
+                //Really Hope this is always 14
+                if (unitBase->UldManager.NodeList[i]->NodeID == id)
+                {
+                    return unitBase->UldManager.NodeList[i];
+                }
+            }
+            return null;
+        }
         private unsafe void DrawUI()
         {
 
@@ -100,9 +112,10 @@ namespace GearSetter
 
             if (ImGui.BeginTabBar("Main"))
             {
-                if (ImGui.BeginTabItem("Job ID Stuff"))
+                if (ImGui.BeginTabItem("Job ID asdfStuff"))
                 {
 
+                    /**
                     //https://ffxiv.pf-n.co/xivapi?url=%2FClassJob
 
                     var gearsetModule = RaptureGearsetModule.Instance();
@@ -124,10 +137,10 @@ namespace GearSetter
                         
                         if(ImGui.CollapsingHeader($"{gearset->ID}: { gearsetName}  {gearset->ClassJob}"))
                         {
-                            //iLevelCSV.GetRow(gearset->Head.ItemID);
-                            //var shit = itemCSV.GetRow(gearset->Head.ItemID);
-                            //var fuck = iLevelCSV.GetRow(shit)
-                            ImGui.Text($"{gearset->Head.ItemID} Head: {itemCSV.GetRow(gearset->Head.ItemID).Name}. Lvl: {itemCSV.GetRow(gearset->Head.ItemID).LevelEquip}");
+                            iLevelCSV.GetRow(gearset->Head.ItemID);
+                            var shit = itemCSV.GetRow(gearset->Head.ItemID);
+                            var poop = shit.LevelItem;
+                            ImGui.Text($"{gearset->Head.ItemID} Head: {itemCSV.GetRow(gearset->Head.ItemID).Name}. Lvl: {itemCSV.GetRow(gearset->Head.ItemID).LevelEquip} iLvl: {poop}");
                             ImGui.Text($"{gearset->Body.ItemID} Body: {itemCSV.GetRow(gearset->Body.ItemID).Name}. Lvl: {itemCSV.GetRow(gearset->Body.ItemID).LevelEquip}");
                             //ImGui.Text($"{gearset->MainHand.ItemID} Main Hand: {itemCSV.GetRow(gearset->MainHand.ItemID).Name}. Lvl: {itemCSV.GetRow(gearset->MainHand.ItemID).LevelEquip}");
                             ImGui.Text($"{gearset->MainHand.ItemID} Main Hand: ");
@@ -135,8 +148,25 @@ namespace GearSetter
 
                         }
 
-                    }
-                    
+                    }*/
+
+                    //Iterate through classes
+                    var characterWindowPtr = gui.GetAddonByName("Character", 1);
+                    var characterWindow = (AtkUnitBase*)characterWindowPtr;
+                    ImGui.Text($"Character Addr:{characterWindowPtr.ToString("X")}");
+
+                   
+
+                    var boundingBox = FindNodeByID(11, characterWindow);
+                    //var gearSetLabel = FindNodeByID(15,characterWindow);
+                    var gearSetSelectedLabel = FindNodeByID(16, characterWindow);
+                    var gearSetLabel = (AtkTextNode*)FindNodeByID(15, characterWindow);
+                    var gearSetLabelEncapulate = FindNodeByID(16, characterWindow);
+                    gearSetLabelEncapulate->X = 1000;
+
+
+
+
                     ImGui.EndTabItem();
 
                 }
